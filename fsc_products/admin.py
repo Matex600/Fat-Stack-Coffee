e0b87398d83a5149718907ee47af351d1400b215
Matex600 <mat.leks@gmail.com>
2022-05-06 22:38:41 +0000

    Add classes to admin.py for products

--- fsc_products/admin.py
+++ fsc_products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductsAdmin)
+admin.site.register(Category, CategoriesAdmin)
