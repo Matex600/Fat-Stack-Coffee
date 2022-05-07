d4b4b9dbce88ad6805d6dff1471cad441776c417
Matex600 <mat.leks@gmail.com>
2022-05-07 20:46:34 +0000

    Added view and urls for product details

--- fsc_products/urls.py
+++ fsc_products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('', views.fsc_products, name='products')
+    path('', views.fsc_products, name='products'),
+    path('<product_id>', views.fsc_product_detail, name='products_detail'),
 ]
--- fsc_products/views.py
+++ fsc_products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -8,5 +8,15 @@ def fsc_products(request):
 
     context = {
         'products': products,
+    }
+    return render(request, 'products/products.html', context)
+
+
+def fsc_product_detail(request, product_id):
+
+    products = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': products,
     }
     return render(request, 'products/products.html', context)
