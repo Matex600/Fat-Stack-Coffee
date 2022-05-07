ec06f3eee1a5ccb953cfd7def1a4b1843dcd9ad6
Matex600 <mat.leks@gmail.com>
2022-05-07 11:03:37 +0000

    Create basic view for products

--- fsc_products/views.py
+++ fsc_products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def fsc_products(request):
+
+    products = Product.object.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
