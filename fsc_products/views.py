39f855ed7b865aad59899eb894f34fb9e97e1dc1
Matex600 <mat.leks@gmail.com>
2022-05-08 11:27:57 +0000

    Added search query to product view

--- fsc_products/views.py
+++ fsc_products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -6,8 +8,23 @@ def fsc_products(request):
 
     products = Product.objects.all()
 
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please try to specify \
+                                         your search query!")
+                return redirect(reverse('products'))
+            
+            queries = (
+                Q(name__icontains=query) | Q(description__icontains=query))
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
     }
     return render(request, 'products/products.html', context)
 
