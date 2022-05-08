701255b66732fc72e07bfca2919647d936c6e98e
Matex600 <mat.leks@gmail.com>
2022-05-08 16:22:29 +0000

    Add logic to views for sorting product

--- fsc_products/views.py
+++ fsc_products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from django.db.models.functions import Lower
+from .models import Product, Category
 
 
 def fsc_products(request):
@@ -9,6 +10,28 @@ def fsc_products(request):
     products = Product.objects.all()
 
     query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'prod_name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('prod_name'))
+   
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+    
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     if request.GET:
         if 'q' in request.GET:
@@ -17,21 +40,26 @@ def fsc_products(request):
                 messages.error(request, "Please try to specify \
                                          your search query!")
                 return redirect(reverse('products'))
-            
+
             queries = (
-                Q(prod_name__icontains=query) | Q(description__icontains=query))
+                Q(prod_name__icontains=query) | Q(description__icontains=query)
+                )
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def fsc_product_detail(request, product_id):
-    
+
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
