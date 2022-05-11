4efb3de47aa8261a6a75f12704b4223885b8f80e
Matex600 <mat.leks@gmail.com>
2022-05-11 15:54:54 +0000

    Add messages to cart views.py

--- fsc_cart/views.py
+++ fsc_cart/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
-
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from fsc_products.models import Product
 
 def view_cart(request):
     return render(request, 'cart/cart.html')
@@ -7,6 +8,7 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     weight = None
@@ -18,15 +20,20 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if weight in cart[item_id]['items_by_weight'].keys():
                 cart[item_id]['items_by_weight'][weight] += quantity
+                messages.success(request, f'Updated weight {weight.upper()} {product.name} quantity to {cart[item_id]["items_by_weight"][weight]}')
             else:
                 cart[item_id]['items_by_weight'][weight] = quantity
+                messages.success(request, f'Added weight {weight.upper()} {product.name} to your shopping cart')
         else:
             cart[item_id] = {'items_by_weight': {weight: quantity}}
+            messages.success(request, f'Added weight {weight.upper()} {product.name} to your shopping cart')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your shopping cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -34,7 +41,7 @@ def add_to_cart(request, item_id):
 
 def update_cart(request, item_id):
 
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     weight = None
     if 'product_weight' in request.POST:
@@ -44,21 +51,27 @@ def update_cart(request, item_id):
     if weight:
         if quantity > 0:
             cart[item_id]['items_by_weight'][weight] = quantity
+            messages.success(request, f'Updated weight {weight.upper()} {product.name} quantity to {cart[item_id]["items_by_weight"][weight]}')
         else:
             del cart[item_id]['items_by_weight'][weight]
             if not cart[item_id]['items_by_weight']:
                 cart.pop(item_id)
+            messages.success(request, f'Removed item{weight.upper()} {product.name} from your shopping cart')
     else:
         if quantity > 0:
             cart[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your shopping cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
 
 def delete_from_cart(request, item_id):
+
+    product = get_object_or_404(Product, pk=item_id)
 
     try:
         weight = None
@@ -70,11 +83,14 @@ def delete_from_cart(request, item_id):
             del cart[item_id]['items_by_weight'][weight]
             if not cart[item_id]['items_by_weight']:
                 cart.pop(item_id)
+            messages.success(request, f'Removed weight {weight.upper()} {product.name} from your shopping cart')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your shopping cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
