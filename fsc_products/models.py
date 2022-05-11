4b0aeeaa40c8d6aabe6de845c663c4eecba27a1b
Matex600 <mat.leks@gmail.com>
2022-05-11 23:08:17 +0000

    Add better spacing to models.py and clear pep8 errors

--- fsc_products/models.py
+++ fsc_products/models.py
@@ -10,7 +10,11 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
@@ -22,16 +26,39 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    sku = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
+    name = models.CharField(
+        max_length=254
+    )
     description = models.TextField()
-    coffee_amounts = models.BooleanField(default=False, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2,
-                                 null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    coffee_amounts = models.BooleanField(
+        default=False,
+        null=True,
+        blank=True
+    )
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    image_url = models.URLField(
+        max_length=1024,
+        null=True,
+        blank=True
+    )
+    image = models.ImageField(
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
-
