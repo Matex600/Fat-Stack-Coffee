65e62921aea34f47f31b6eb178614a3ba7bbf4f5
Matex600 <mat.leks@gmail.com>
2022-05-13 18:00:25 +0000

    Created checkout forms.py

---
+++ fsc_checkout/forms.py
@@ -0,0 +1,59 @@
+"""
+
+checkout/forms.py: forms for the checkout app.
+Credit to Code Institute's Boutique Ado project.
+
+"""
+
+# - - - - - Django Imports - - - - - - - - -
+from django import forms
+
+# - - - - - Internal Imports - - - - - - - - -
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    """
+    Form to manage user info on order placement.
+    """
+    class Meta:
+        model = Order
+        fields = (
+            'full_name',
+            'email',
+            'phone_number',
+            'street_address1',
+            'street_address2',
+            'town_or_city',
+            'postcode',
+            'country',
+            'county',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County, State, Province',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
