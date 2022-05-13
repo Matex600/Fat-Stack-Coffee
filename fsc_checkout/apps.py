7c5b153ae542beb1f6158fd49af57ce15758ea57
Matex600 <mat.leks@gmail.com>
2022-05-13 14:28:21 +0000

    Create signals.py and update apps.py in checkout app

--- fsc_checkout/apps.py
+++ fsc_checkout/apps.py
@@ -1,6 +1,18 @@
+"""
+
+checkout/apps.py: config file for checkout app
+
+"""
+
+
+# - - - - - Django Imports - - - - - - - - -
 from django.apps import AppConfig
 
 
 class FscCheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fsc_checkout'
+
+    def ready(self):
+        # pylint: disable=unused-import, import-outside-toplevel
+        import fsc_checkout.signals # noqa
---
+++ fsc_checkout/signals.py
@@ -0,0 +1,28 @@
+"""
+
+checkout/signals.py: config file for home app
+Credit to Code Institute's Boutique Ado project.
+
+"""
+
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
