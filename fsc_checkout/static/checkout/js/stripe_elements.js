e449ee1203ab7e9b1c7d8184b50991f5143090a6
Matex600 <mat.leks@gmail.com>
2022-05-14 15:00:54 +0000

    Create stripe_elements.js for styling and functionality of elements

---
+++ fsc_checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,30 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Lato", Montserrat, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
