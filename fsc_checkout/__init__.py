9a2b0272719b423bb442ad0355af8f1daa6e1118
Matex600 <mat.leks@gmail.com>
2022-05-15 13:21:57 +0000

    Update __init__.py file to connect to apps.py

---
+++ fsc_checkout/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'fsc_checkout.apps.FscCheckoutConfig'
--- fsc_checkout/apps.py
+++ fsc_checkout/apps.py
@@ -10,7 +10,6 @@ from django.apps import AppConfig
 
 
 class FscCheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'fsc_checkout'
 
     def ready(self):
