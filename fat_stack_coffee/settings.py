89255d019a022dab5db6f3c2e411761764fc32a8
Matex600 <mat.leks@gmail.com>
2022-05-16 16:41:25 +0000

    Add css to overlay loader

--- fat_stack_coffee/settings.py
+++ fat_stack_coffee/settings.py
@@ -197,7 +197,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
