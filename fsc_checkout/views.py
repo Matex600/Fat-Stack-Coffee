3db49a338625445822dbdd69fa87944e22977ab8
Matex600 <mat.leks@gmail.com>
2022-05-16 20:36:18 +0000

    Add view to cache checkout data and urls

--- fsc_checkout/views.py
+++ fsc_checkout/views.py
@@ -10,6 +10,7 @@ Boutique Ado project.
 from django.shortcuts import (
     render, redirect, reverse, get_object_or_404, HttpResponse
 )
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -21,6 +22,23 @@ from fsc_cart.contexts import cart_contents
 
 # - - - - - 3rd party imports - - - - - - - - - - - -
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
