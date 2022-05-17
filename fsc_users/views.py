8a99b7678fe0d7b210b4ac668e4111d27845e56d
Matex600 <mat.leks@gmail.com>
2022-05-17 14:50:33 +0000

    Create view and urls for fsc_users

--- fsc_users/views.py
+++ fsc_users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def user_profile(request):
+    """ Display the user's profile. """
+
+    template = 'user_profiles/user_profile.html'
+    context = {}
+
+    return render(request, template, context)
