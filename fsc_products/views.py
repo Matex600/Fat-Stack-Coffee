5abca078e17c89bec4beaaff6292bace5748f4c5
Matex600 <mat.leks@gmail.com>
2022-05-18 19:52:44 +0000

    Add if statement to enable toasts when adding product and validate form

--- fsc_products/views.py
+++ fsc_products/views.py
@@ -83,7 +83,16 @@ def fsc_product_detail(request, product_id):
 
 def fsc_add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('fsc_add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
