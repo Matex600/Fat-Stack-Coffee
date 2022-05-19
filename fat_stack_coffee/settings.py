f64a52da68fe24184b1a16df815adb1b200c726b
Matex600 <mat.leks@gmail.com>
2022-05-19 21:08:48 +0000

    Fix pylint errors in settings.py

--- fat_stack_coffee/settings.py
+++ fat_stack_coffee/settings.py
@@ -20,6 +20,8 @@ if os.path.isfile('env.py'):
     error if it cannot find env
     """
     import env
+    # 'env' imported but unused is because the application
+    # only looks for it when it needs it.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,7 +150,6 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-    
 
 
 # Password validation
@@ -156,16 +157,20 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
