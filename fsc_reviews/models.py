709f33c359bc88df8ba652ddf0d6ca641b83de85
Matex600 <mat.leks@gmail.com>
2022-05-20 16:26:43 +0000

    Create model, add comments for fsc_reviews app

--- fsc_reviews/models.py
+++ fsc_reviews/models.py
@@ -1,3 +1,26 @@
+"""
+
+fsc_reviews/models.py: Contains the model for user reviews.
+
+"""
+
+# - - - - - Django Imports - - - - - - - - -
 from django.db import models
 
-# Create your models here.
+# - - - - - Internal Imports - - - - - - - - -
+from fsc_products.models import Product
+from fsc_users.models import UserProfile
+
+
+class Review(models.Model):
+    """
+    The review model class, with fields for
+    user and products using a foreign key (unique value)
+    A prod_description and review_date
+    """
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    prod_desc = models.TextField(max_length=450, null=False,
+                                 blank=False)
+    review_time = models.DateTimeField(auto_now_add=True)
