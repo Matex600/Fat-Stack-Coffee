23c2096faab43fa6fa6971d63dcdfb5d54cca5b5
Matex600 <mat.leks@gmail.com>
2022-05-20 17:13:06 +0000

    Create forms.py for fsc_reviews app

---
+++ fsc_reviews/forms.py
@@ -0,0 +1,18 @@
+"""
+
+fsc_reviews/forms.py: Contains the form class for user reviews.
+
+"""
+
+# - - - - - Django Imports - - - - - - - - -
+from django import forms
+
+# - - - - - Internal Imports - - - - - - - - -
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    """ Form for users to add reviews of products"""
+    class Meta:
+        model = Review
+        fields = ('prod_desc',)
