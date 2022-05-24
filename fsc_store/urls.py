d1a8ddf141bc7cc0f162cd6892470db8b3f5b1ce
Matex600 <mat.leks@gmail.com>
2022-05-24 00:04:28 +0000

    Create about_us page for fat-stack-coffee

--- fsc_store/urls.py
+++ fsc_store/urls.py
@@ -8,5 +8,6 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('', views.index, name='store')
+    path('', views.index, name='store'),
+    path('about/', views.about, name='about'),
 ]
--- fsc_store/views.py
+++ fsc_store/views.py
@@ -13,3 +13,10 @@ def index(request):
     Class to display the landing page of the site, index.html.
     """
     return render(request, 'store/index.html')
+
+
+def about(request):
+    """
+    Class to display the about page of the site, about_us.html.
+    """
+    return render(request, 'about/about_us.html')
