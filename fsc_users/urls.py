acb03e0a151fb23a5fd1defb009c402648bee153
Matex600 <mat.leks@gmail.com>
2022-05-26 15:48:18 +0000

    Add urls for add and delete favourites to fsc_users.urls.py

--- fsc_users/urls.py
+++ fsc_users/urls.py
@@ -8,10 +8,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_profile, name='profile'),
+    path(
+        '',
+        views.user_profile,
+        name='profile'
+    ),
     path(
         'order_history/<order_number>',
         views.order_history,
         name='order_history'
+    ),
+    path(
+        'add_to_favourites/<int:product_id>',
+        views.add_to_favourites,
+        name='add_to_favourites'
+    ),
+    path(
+        'delete_from_favourites/<int:product_id>',
+        views.delete_from_favourites,
+        name='delete_from_favourites'
     ),
 ]
