4e3e6b9254bc32d58c7a663047fc990f87f478bb
Matex600 <mat.leks@gmail.com>
2022-05-28 17:36:08 +0000

    Test contact page bug attempt#5

--- fat_stack_coffee/settings.py
+++ fat_stack_coffee/settings.py
@@ -34,8 +34,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = "DEVELOPMENT" in os.environ
-DEBUG = False
+DEBUG = "DEVELOPMENT" in os.environ
+
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
