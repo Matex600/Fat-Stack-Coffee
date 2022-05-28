cb31985b7d1bda5dbc8983a65c62c1d64f60d6bd
Matex600 <mat.leks@gmail.com>
2022-05-28 14:25:07 +0000

    Add Jazzmin to customise admin and freeze requirements

--- fat_stack_coffee/settings.py
+++ fat_stack_coffee/settings.py
@@ -44,6 +44,7 @@ ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME', 'localhost')]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -230,3 +231,37 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "FSC_ADMIN",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Fat Stack Coffee Admin",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "FSC Admin Panel",
+
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": None,
+
+    # Logo to use for login form in dark themes (defaults to login_logo)
+    "login_logo_dark": None,
+
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
+
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome Coffee Admin",
+
+    # Copyright on the footer
+    "copyright": "Mateusz Leks 2022, FatStackCoffee, Django, Educational Purposes Only",
+
+    # The model admin to search from the search bar, search bar omitted if excluded
+    "search_model": "auth.User",
+
+}
